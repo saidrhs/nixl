44466f80fd9232570e51740cd275182ad8dd84ed
Ilia Yastrebov <iyastrebov@nvidia.com>
2025-07-25 08:17:19 +0000

    Set small chunks in test

--- test/gtest/test_transfer.cpp
+++ test/gtest/test_transfer.cpp
@@ -438,6 +438,10 @@ const std::string TestTransfer::NOTIF_MSG = "notification";
 
 TEST_P(TestTransfer, RandomSizes)
 {
+    // TODO: pass as backend params?
+    setenv("NIXL_MIN_CHUNK_SIZE", "32", 1);
+    setenv("NIXL_MAX_CHUNK_SIZE", "128", 1);
+
     // Tuple fields are: size, count, repeat, num_threads
     constexpr std::array<std::tuple<size_t, size_t, size_t, size_t>, 4> test_cases = {
         {{4096, 8, 3, 1},
@@ -526,10 +530,10 @@ INSTANTIATE_TEST_SUITE_P(ucx_no_pt,
                          testing::Values(std::make_tuple("UCX", false, 2, 0)));
 INSTANTIATE_TEST_SUITE_P(ucx_threadpool,
                          TestTransfer,
-                         testing::Values(std::make_tuple("UCX", false, 8, 4)));
+                         testing::Values(std::make_tuple("UCX", false, 6, 4)));
 INSTANTIATE_TEST_SUITE_P(ucx_threadpool_no_pt,
                          TestTransfer,
-                         testing::Values(std::make_tuple("UCX", false, 8, 4)));
+                         testing::Values(std::make_tuple("UCX", false, 6, 4)));
 INSTANTIATE_TEST_SUITE_P(ucx_mo,
                          TestTransfer,
                          testing::Values(std::make_tuple("UCX_MO", true, 2, 0)));
